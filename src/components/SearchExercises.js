7fa28280620e46e3a2b2864c050b349e2d79b87c
maxwell wong <midorikawamaxwell@gmail.com>
2023-04-01 17:01:29 +0900

    added title and search bar for the Search exercise component

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {Box, Button, Stack, TextField, Typography} from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <div>
-      Search Exercises
-    </div>
+    <Stack alignItems='center' mt='37px' justifyContent='center'
+    p='20px'>
+      <Typography fontWeight={700} sx={{fontSize: {lg: '44px', xs: '30px'}}} mb='50px' textAlign='center'>
+        Awesome Exercises You <br /> Should Know
+      </Typography>
+      <Box position='relatiev' mb='72px'>
+        <TextField
+          sx={{
+            input: {fontWeight: '700px', border: 'none', borderRadius: '4px'},
+            width: {lg: '1170px', xs: '350px'},
+            backgroundColor: '#fff',
+            borderRadius: '40px'
+          }}
+          height='75px'
+          value=''
+          onChange={(e) => {}}
+          placeholder='Search Exercises'
+          type='text'
+        >
+
+        </TextField>
+      </Box>
+    </Stack>
   );
 };
 

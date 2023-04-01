ed4f83d5173fa967150ba98ede618a4740a8caa8
maxwell wong <midorikawamaxwell@gmail.com>
2023-04-01 20:08:20 +0900

    created bodypart exercise cards for horizontal scroll with icon image

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Stack, Typography} from '@mui/material';
 
-const BodyPart = () => {
+import Icon from '../assets/icons/gym.png';
+
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
-    <div>
-
-    </div>
+    <Stack
+     type='button'
+     alignItems='center'
+     justifyContent='center'
+     className='bodyPart-card'
+     sx={
+        bodyPart === item ? {
+          borderTop: '4px solid#ff2625',
+          backgroundColor: '#fff',
+          borderBottomLeftRadius: '20px',
+          width: '270px',
+          height: '280px',
+          cursor: 'pointer',
+          gap: '47px'
+        } : ''
+    }
+    >
+      <img src={Icon} alt='dumbbell' styles={{ width: '40px', height: '40px'}}/>
+    </Stack>
   );
 };
 

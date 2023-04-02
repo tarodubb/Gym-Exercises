17b6669c63bb42df132e0a6d1adb13791b0994a8
maxwell wong <midorikawamaxwell@gmail.com>
2023-04-02 16:58:11 +0900

    made it so that the category filters the workouts search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,22 +4,16 @@ import { Box } from '@mui/material';
 import Exercises from '../components/Exercises';
 import SearchExercises from '../components/SearchExercises';
 import HeroBanner from '../components/HeroBanner';
+
 const Home = () => {
-  const [bodyPart, setbodyPart] = useState('all');
   const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises
-        setExercises={setExercises}
-        bodyPart={bodyPart}
-        setbodyPart={setbodyPart}
-      />
-      <Exercises
-        setExercises={setExercises}
-        bodyPart={bodyPart}
-        exercises={exercises}
-      />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
     </Box>
   );
 };

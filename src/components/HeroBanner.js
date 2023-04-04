05b8bf24d3f97cf12c0565e78836a1c14e90d1ac
maxwell wong <midorikawamaxwell@gmail.com>
2023-04-04 20:27:39 +0900

    margin and image corner changes for hero banner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -10,20 +10,20 @@ const HeroBanner = () => {
       ml: { sm: '50px'}
       }} position='relative' p='20px'
     >
-      <Typography color='#d32f2f' fontWeight='600' fontSize='26px'>
+      <Typography color='#d32f2f' fontWeight='600' fontSize='26px' ml='40px'>
         Fitness Club
       </Typography>
       <Typography fontWeight={700}
-       sx ={{ fontSize: { lg: '44px', xs: '40px' }
+       sx ={{ fontSize: { lg: '54px', xs: '40px' }
       }}
-      mb='23px' mt='30px'
+      mb='23px' mt='30px' ml='40px'
       >
         Sweat, Smile <br /> and Repeat
       </Typography>
-      <Typography fontSize='22px' lineHeight='35px' mb={4}>
+      <Typography fontSize='22px' lineHeight='35px' mb={4} ml='40px'>
         Check out the most effective exercises
       </Typography>
-      <Button variant='contained' color='error' href='#exerciseSearch' className='smooth-scroll'>Explore Exercises</Button>
+      <Button variant='contained' color='error' href='#exerciseSearch' className='smooth-scroll' style={{ marginLeft: '40px' }}>Explore Exercises</Button>
       <img src={HeroBannerImage} alt='banner' className='hero-banner-img'/>
       <Typography fontWeight={600}
         color='#ff2625'

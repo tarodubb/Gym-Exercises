21b73741ddca2c7c34f503f030539a4eb6568a33
maxwell wong <midorikawamaxwell@gmail.com>
2023-04-21 16:43:51 +0900

    added a search state to the search input

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react';
 import {Box, Button, Stack, TextField, Typography} from '@mui/material';
 
 const SearchExercises = () => {
+  const [search, setSearch] = useState('')
+
   return (
     <Stack alignItems='center' mt='37px' justifyContent='center'
     p='20px'>
       <Typography fontWeight={700} sx={{fontSize: {lg: '44px', xs: '30px'}}} mb='50px' textAlign='center'>
         Awesome Exercises You <br /> Should Know
       </Typography>
-      <Box position='relatiev' mb='72px'>
+      <Box position='relative' mb='72px'>
         <TextField
           sx={{
             input: {fontWeight: '700px', border: 'none', borderRadius: '4px'},
@@ -17,8 +19,8 @@ const SearchExercises = () => {
             borderRadius: '40px'
           }}
           height='75px'
-          value=''
-          onChange={(e) => {}}
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase)}
           placeholder='Search Exercises'
           type='text'
         >
@@ -31,7 +33,8 @@ const SearchExercises = () => {
           width: { lg: '175px', xs: '80px'},
           fontSize: { lg: '20px', xs: '14px'},
           height: '56px',
-          position: 'absolute'
+          position: 'absolute',
+          right: '0'
          }}
         >
             Search
